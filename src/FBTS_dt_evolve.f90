5d8972a6d9fdd9bd670649344138a3f19a40f8d9
Shunsuke Sato <shunsuke.sato26@gmail.com>
2017-03-02 16:36:54 +0100

    Implemented a new propagator.

--- src/FBTS_dt_evolve.f90
+++ src/FBTS_dt_evolve.f90
@@ -3,6 +3,87 @@
 ! Released under the MIT license                    !
 ! https://opensource.org/licenses/mit-license.php   !
 !---------------------------------------------------!
+subroutine FBTS_dt_evolve_quantum
+  use global_variables
+  implicit none
+  real(8) :: n_m(Lsite),n_n(Lsite),n(Lsite)
+  real(8) :: Htot_t(Lsite,Lsite)
+  complex(8) :: z_m(Lsite),z_n(Lsite)
+  integer :: i,j
+  complex(8) :: z_t(Lsite),zh_t(Lsite),zfact
+  integer,parameter :: NTaylor=6
+  integer :: iexp
+
+
+  z_m = x_m + zI * p_m
+  z_n = x_n + zI * p_n
+
+  do i = 1,Lsite
+    n_m(i) = 0.5d0*(x_m(i)**2 + p_m(i)**2)
+    n_n(i) = 0.5d0*(x_n(i)**2 + p_n(i)**2)
+!    n(i) = n_m(i) + n_n(i) ! with trace
+    n(i) = n_m(i) + n_n(i) - 1.0d0 ! traceless
+    F_HO(i) = gamma*sqrt(2d0*mass*omega0)*n(i) - omega0**2*mass*X_HO(i) 
+  end do
+
+  V_HO = V_HO +0.5d0*dt*F_HO/mass
+
+
+  zfact = 1d0
+  z_t = z_m
+  do iexp = 1,NTaylor
+    zfact = zfact*(-zI*dt*0.5d0)/iexp
+    call hpsi(z_t,zh_t)
+    z_m = z_m + zfact*zh_t
+    z_t = zh_t
+  end do
+
+  zfact = 1d0
+  z_t = z_n
+  do iexp = 1,NTaylor
+    zfact = zfact*(-zI*dt*0.5d0)/iexp
+    call hpsi(z_t,zh_t)
+    z_n = z_n + zfact*zh_t
+    z_t = zh_t
+  end do
+
+
+  X_HO = X_HO + V_HO*dt
+
+  zfact = 1d0
+  z_t = z_m
+  do iexp = 1,NTaylor
+    zfact = zfact*(-zI*dt*0.5d0)/iexp
+    call hpsi(z_t,zh_t)
+    z_m = z_m + zfact*zh_t
+    z_t = zh_t
+  end do
+
+  zfact = 1d0
+  z_t = z_n
+  do iexp = 1,NTaylor
+    zfact = zfact*(-zI*dt*0.5d0)/iexp
+    call hpsi(z_t,zh_t)
+    z_n = z_n + zfact*zh_t
+    z_t = zh_t
+  end do
+
+
+  x_m = real(z_m); p_m = aimag(z_m)
+  x_n = real(z_n); p_n = aimag(z_n)
+
+  do i = 1,Lsite
+    n_m(i) = 0.5d0*(x_m(i)**2 + p_m(i)**2 )
+    n_n(i) = 0.5d0*(x_n(i)**2 + p_n(i)**2 )
+!    n(i) = n_m(i) + n_n(i) ! with trace
+    n(i) = n_m(i) + n_n(i) - 1.0d0 ! traceless
+    F_HO(i) = gamma*sqrt(2d0*mass*omega0)*n(i) - omega0**2*mass*X_HO(i) 
+  end do
+
+  V_HO = V_HO +0.5d0*dt*F_HO/mass
+
+end subroutine FBTS_dt_evolve_quantum
+!======================================================================================
 subroutine FBTS_dt_evolve
   use global_variables
   implicit none
@@ -51,6 +132,7 @@ subroutine FBTS_dt_evolve
   V_HO = V_HO +0.5d0*dt*F_HO/mass
 
 end subroutine FBTS_dt_evolve
+!==========================================================================
 subroutine FBTS_calc_ene(Etot0)
   use global_variables
   implicit none
